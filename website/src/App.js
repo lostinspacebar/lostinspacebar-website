178e1c6a36202b75cdc26e4c756c8190b02bc8fa
Aditya Gaddam <adityagaddam@gmail.com>
2020-12-29 12:16:26 -0500

    Tweaked transition group to reduce any ghost clicks on stuff still under transition.
    Signed-off-by: Aditya Gaddam <adityagaddam@gmail.com>

--- website/src/App.js
+++ website/src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
 import React from 'react';
-import { Link, BrowserRouter, Switch, Route} from 'react-router-dom';
+import { Link, BrowserRouter, Switch, Route, useLocation } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import HomePage from './components/HomePage';
 import TextCycler from './components/TextCycler';
 import ProjectListPage from './components/ProjectListPage';
 
 function App() {
-    const interests = ['\'m an engineer', '\'m a developer', '\'m a designer', ' like to sleep in the woods', ' make my own gear', ' like to run in the rain', ' like to bike in the dirt', ' like to snowboard', '\'m ok at archery']
     return (
         <BrowserRouter>
+            <AppContents />
+        </BrowserRouter>
+    );
+}
+
+function AppContents() {
+    const interests = ['\'m an engineer', '\'m a developer', '\'m a designer', ' like to sleep in the woods', ' make my own gear', ' like to run in the rain', ' like to bike in the dirt', ' like to snowboard', '\'m ok at archery']
+    const location = useLocation();
+
+    return (
         <div id="main-container">
             <header>
                 <h1>
-                    <Link className="main-logo" to="/" title="LOST.IN.SPACEBAR 2020"><span className="hidden">LOST.IN.SPACEBAR</span></Link>
+                    <Link className="main-logo" to='/' title="LOST.IN.SPACEBAR 2020"><span className="hidden">LOST.IN.SPACEBAR</span></Link>
                 </h1>
                 <p className="brief">
                     <b>Hello! My name is Aditya Gaddam.<br />I<TextCycler choices={interests} />.</b><br />
@@ -21,25 +30,20 @@ function App() {
                 </p>
             </header>
             <main>
-                <Route render={({location}) => {
-                    return (
-                        <TransitionGroup className="main-content-transition-group">
-                            <CSSTransition key={location.key} timeout={600} classNames="content-transition">
-                                <div className="main-content-transition-container">
-                                    <Switch location={location}>
-                                        <Route path="/projects" component={ProjectListPage} />
-                                        <Route path="/" component={HomePage} />
-                                    </Switch>
-                                </div>
-                            </CSSTransition>
-                        </TransitionGroup>
-                    );
-                }} />
+                <TransitionGroup className="main-content-transition-group">
+                    <CSSTransition key={location.key} timeout={75} classNames="content-transition">
+                        <div className="main-content-transition-container">
+                            <Switch location={location}>
+                                <Route path="/projects" component={ProjectListPage} />
+                                <Route path="/" component={HomePage} />
+                            </Switch>
+                        </div>
+                    </CSSTransition>
+                </TransitionGroup>
             </main>
             <footer>
             </footer>
         </div>
-        </BrowserRouter>
     );
 }
 
